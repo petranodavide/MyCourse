4a0335a83419be0cbd3ebb6cf5a55ae423a0b9c5
Davide Petrano <davide.petrano@gmail.com>
2020-11-19 14:51:02 +0100

    Primo commit

---
+++ src/MyCourse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstArgument = args.FirstOrDefault();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
